cf5cad8f80ef857a377e59e895e18c903d8b602e
root <root@DESKTOP-6GG0TFB.localdomain>
2026-07-05 12:24:38 +0000

    Some docker practice

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("Program is running...")
+
+# Your background tasks or logic here
+print("Working...")
+
+# The program pauses here until the user presses Enter
+input("Press Enter to exit the program...")
+
+print("Program stopped!")
+
